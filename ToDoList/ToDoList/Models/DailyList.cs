b587698da1a6e25e70bce6b23cbf73ea807ba742
Ana <anaxjovanovicx@gmail.com>
2022-11-21 19:25:07 +0100

    Initial setup

---
+++ ToDoList/ToDoList/Models/DailyList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+    public class DailyList
+    {
+        public int Id { get; set; }
+        public int ToDoListId { get; set; }
+        public IList<Task> Tasks { get; set; }
+    }
+}
---
+++ ToDoList/ToDoList/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public int DailyListId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool Done { get; set; }
+    }
+}
